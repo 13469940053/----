94136fcf435c945350c32d78d55d87fe06c740c3
张成 <44461585+13469940053@users.noreply.github.com>
2019-07-09 03:51:47 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *
+ * @authors zhangcheng (2508723631@qq.com)
+ * @date    2019-06-25 20:50:50
+ */
+?>
+
+
+<!-- <link rel="stylesheet" type="text/css" href="static/style.css"> 测试时使用-->
+<div class="footer">
+<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="330" height="200" src="//music.163.com/outchain/player?type=0&amp;id=616904413&amp;auto=0&amp;height=430"></iframe>
+    <h1>
+        底部
+    </h1>
+        <!--底部内容-->
+    <!-- <p>© Copyright 2019 wulangwang All Rights Reserved.</p> -->
+</div>
+<script>
+    console.log("%c%c源码作者%c无风不起浪@你", "line-height:28px;", "line-height:28px;border-radius:5px 0 0 5px;padding:4px;background:#222;color:#FADFA3;font-size:14px;", "padding:4px;background:#FADFA3;color:#000;line-height:28px;font-size:14px;border-radius:0 5px 5px 0;");
+
+    console.log("%c%c网站地址%c http://www.wulangwang.top", "line-height:28px;", "line-height:28px;padding:4px;background:#222;color:#FADFA3;font-size:14px;", "padding:6px 6px 6px 2px;background:#FADFA3;color:#0093fff;line-height:28px;font-size:12px;");
+
+    console.log("%c就知道阻挡不住你观看源码的冲动，但你至少要记住我！-----%c风不起浪@你", "line-height:28px;", "line-height:28px;padding:4px 0px;color:#fff;font-size:16px;background-image:-webkit-gradient(linear,left top,right top,color-stop(0,#ff22ff),color-stop(1,#5500ff));color:transparent;-webkit-background-clip: text;");
+</script>
+<script>
+    // <!--百度收录自动推送-->
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+//360自动提交连接-->
+ (function(){
+        var src = (document.location.protocol == 'http:') ? 'http://js.passport.qihucdn.com/11.0.1.js?d4d9f371abe1a3935500b5aaf98e676d':'https://jspassport.ssl.qhimg.com/11.0.1.js?d4d9f371abe1a3935500b5aaf98e676d';
+        document.write('<script src=' + src + ' id='sozz'><\/script>');
+        })();
+</script>
---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @authors zhangcheng (2508723631@qq.com)
+ * @date    2019-06-25 20:50:50
+ */
+?>
+
+
+<!-- <link rel="stylesheet" type="text/css" href="static/style.css"> 测试时使用-->
+<header>
+    <h1>头部</h1>
+
+</header>
